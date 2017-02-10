dd079c78be1c5026cac2664a77d5a62fa9d97e9c
lalida <s.skinnykidz@gmail.com>
2017-02-10 11:22:59 +0700

    global style

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -14,6 +14,8 @@ require('./bootstrap');
  */
 
 Vue.component('example', require('./components/Example.vue'));
+Vue.component('modal', require('./components/Modal.vue'));
+Vue.component('loginmodal', require('./components/LoginModal.vue'));
 
 const app = new Vue({
     el: '#app'
